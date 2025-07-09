c492467a066361c927c03a48abd804d137847476
GabrielaSales-23 <gabrielasales@discente.ufg.br>
2025-07-09 16:54:19 -0300

    Removing git script and updating gitignore

--- R/01_git.R
+++
@@ -1,9 +0,0 @@
-library(devtools)
-library(gitcreds)
-library(usethis)
-
-usethis::create_project(getwd())
-
-usethis::use_git()
-
-use_git_config("GabrielaSales-23", "gabrielasales@discente.ufg.br")
