d5c948a35434db45a80b93a085df0c4a8c813e30
GabrielaSales-23 <gabrielasales@discente.ufg.br>
2025-07-09 15:50:42 -0300

    Removing unused analyses from script 02

--- R/02_Analyzes_projects-distribution.R
+++ R/02_Analyzes_projects-distribution.R
@@ -6,7 +6,7 @@
 # Sum countries' projects
 library(dplyr)
 country_count <- df %>% count(Country)
-country_count <- country_count %>% 
+country_count <- country_count %>%
   rename(region = Country)
 country_count <- country_count[1:35,]
 View(country_count)
@@ -31,27 +31,7 @@ country_count <- country_count %>%
 mapdata = left_join(mapdata, country_count, by="region")
 View(mapdata)
 
-#mapdata1 = mapdata %>% filter(n >1)
 
-#Ploting 
-# mapdata %>%
-#   mutate(n = if_else(is.na(n), 0L, n)) %>%
-#   ggplot(aes(x=long, y=lat, group=group,fill=n))+
-#   geom_polygon(color="black",linewidth=0.2)+
-#   scale_fill_gradient(name= "Projects",
-#                       low="#FFFFFF", high = "#FF0000",
-#                       limits=c(0,10),
-#                       guide = guide_colourbar(title.position = "top"))+
-#   
-#   coord_quickmap()+
-#   theme_void()+
-#   theme(legend.title.align = 0.5,
-#         legend.box.just = "center")
-# ggsave("Figures/map.png",height = 12,width = 8, units = "in",dpi = 300)
-
-library(viridis)
-library(ggpattern)
-library(scales)
 
 ##Establishing climate zones
 # Carregar pacotes necessários
@@ -62,6 +42,9 @@ library(terra)
 library(sf)
 library(rnaturalearthdata)
 library(dplyr)
+library(viridis)
+library(ggpattern)
+library(scales)
 
 # Transformar o 'mapdata' em um objeto 'sf' para cálculo do centroide
 centroides <- st_as_sf(mapdata, coords = c("long", "lat"), crs = 4326, agr = "constant")
@@ -85,40 +68,40 @@ mapdata <- mapdata %>%
 names(mapdata)
 View(mapdata)
 
-# Atribuindo valor de 'n' conforme a zona climática
+# Adding a value 'n' according to climate zone
 mapdata <- mapdata %>%
   mutate(n = case_when(
-    !is.na(n) & n != 0 ~ n,  # Se n não for NA e não for 0, mantém o valor de 'n'
-    is.na(n) & climate_zone %in% c("tropical", "subtropical") ~ 0L,  # Se n for NA e a zona for tropical ou subtropical, atribui 0
-    is.na(n) & climate_zone %in% c("temperate", "cold") ~ NA_integer_,  # Se n for NA e a zona for temperada ou fria, atribui NA
-    TRUE ~ n  # Mantém o valor de 'n' caso não se encaixe em nenhuma condição acima
+    !is.na(n) & n != 0 ~ n,  # if n is not NA or 0, keep the value 'n'
+    is.na(n) & climate_zone %in% c("tropical", "subtropical") ~ 0L,  # if n is not NA and climate zone is tropical or subtropical, put 0
+    is.na(n) & climate_zone %in% c("temperate", "cold") ~ NA_integer_,  # if n is NA or the climate zone is temperate or cold, put NA
+    TRUE ~ n  # Keep value 'n' if it is any of the above options
   ))
 
 #Ploting
 
-# Separando dados
+# Separating data
 map_na <- mapdata %>% filter(is.na(n))
 map_not_na <- mapdata %>% filter(!is.na(n))
 
-# Verificando o vetor de cores
+# Verifying color vector
 if (!exists("cores")) {
   cores <- viridis::viridis(length(unique(na.omit(mapdata$n))))
 }
 
-# Gráfico
+# Graphic
 ggplot() +
-  # Países com NA - fundo cinza
+  # Countries with NA - gray
   geom_polygon(data = map_na,
                aes(x = long, y = lat, group = group),
                fill = alpha("gray", 0.05),
                color = "black", linewidth = 0.2) +
-  
-  # Países com dados
+
+  # Countries with n different from NA
   geom_polygon(data = map_not_na,
                aes(x = long, y = lat, group = group, fill = factor(n)),
                color = "black", linewidth = 0.2) +
-  
-  # Países com NA - padrão listrado
+
+  # Coutrines with NA - striped pattern
   ggpattern::geom_polygon_pattern(data = map_na,
                                   aes(x = long, y = lat, group = group),
                                   color = "black", linewidth = 0.2,
@@ -126,7 +109,7 @@ ggplot() +
                                   pattern_fill = "gray",
                                   pattern_density = 0.1,
                                   pattern_spacing = 0.02) +
-  
+
   scale_fill_manual(name = "Projects",
                     values = cores,
                     guide = guide_legend(title.position = "top")) +
@@ -136,36 +119,3 @@ ggplot() +
         legend.box.just = "center")
 
 ggsave("Outputs/Figures/mapCorrect5.png",height = 12,width = 8, units = "in",dpi = 300)
-
-# # Acumulation curve -------------------------------------------------------
-# 
-# colnames(df)
-# New_data <- data.frame(df[,c("Year","Country")])
-# plot(New_data)
-# 
-# library(dplyr)
-# library(tidyr)
-# 
-# New_data_resumo <- New_data %>%
-#   group_by(Country, Year) %>%
-#   summarise(n_projects = n(), .groups = "drop")
-# 
-# New_data_acumulado <- New_data %>%
-#   group_by(Country, Year) %>%
-#   summarise(n_projects = n(), .groups = "drop") %>%
-#   arrange(Country, Year) %>%
-#   group_by(Country) %>%
-#   mutate(cumulative_projects = cumsum(n_projects))
-# 
-# 
-# library(ggplot2)
-# 
-# ggplot(New_data_acumulado, aes(x = Year, y = cumulative_projects, color = Country, group = Country)) +
-#   geom_line(linewidth = 1) +
-#   geom_point() +
-#   labs(
-#     x = "Ano",
-#     y = "Projetos acumulados",
-#     title = "Número acumulado de projetos por país ao longo do tempo"
-#   ) +
-#   theme_classic()
